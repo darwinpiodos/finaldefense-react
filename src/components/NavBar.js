ee089f4bdb3b88f31ebad0ddbf7ad2bba7a7c9c3
darwinpiodos <darwindivinopiodos21@gmail.com>
2022-05-15 23:27:22 +0800

    HOme Page pa Intawn ga trabaho

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,64 +31,79 @@ function NavBar(){
 
     return(
         <div id="navbar" className=" container-fluid d-flex align-items-center">
-            
 
             <nav class="navbar navbar-expand-lg navbar-light p-4" style={{display:"table", width:"100%"}}>
-                <div class="container-fluid p-0">
-
-                    
-
-                    <img style={{width:40, marginLeft:70, marginRight:15}}src={Logo} />
-
-                    <a class="navbar-brand" href="#">
-                        
-                     
+                  
+                <div className='row'>
+                    <div className='col-lg-9 d-flex align-items-center'>
+                        <a class="navbar-brand d-flex" href="#">
+                            
+                        <img style={{width:40, marginLeft:70, marginRight:15}}src={Logo} />
                         <p id="logo-name">MLGCL <br></br><span style={{fontSize:18}}>COVID-19 CONTACT TRACING SYSTEM</span></p>
-                        
-                    </a>
+                            
+                        </a>
 
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style={{fontSize:"20px"}}>
                         <span class="navbar-toggler-icon"></span>
                         </button>
+                    </div>
 
-                    <div className="collapse navbar-collapse" id="navbarNav" style={{position:"relative",left:"10px", width:"100%", float:"right"}}>
+                    <div className='col-lg-3'>
 
+                        <div className="collapse navbar-collapse" id="navbarNav" style={{position:"relative",left:"10px", width:"100%", float:"center"}}>
 
+                            <ul class="navbar-nav font-family flex-end" style={{display:"block", position:"relative", left:"10px", top:"10px", width:"99%"}} >
 
-                        <ul class="navbar-nav" style={{position:"relative", left:"30px", top:"10px"}} >
-                            <li class="nav-item" >
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Profile</a>
-                            </li>
+                                {
+                                localStorage.getItem('user-info') ?
+                                <>
+                                    <li class="nav-item" >
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="#">Dashboard</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="#">Profile</a>
+                                    </li>
 
-                            <li class="nav-item" >
-                            <a class="nav-link" href="#">Login</a>
-                            </li>
+                                </>
+                                :
+                                <>
+                                    <li class="nav-item" >
+                                    <a class="nav-link" href="#">Login</a>
+                                    </li>
 
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Register</a>
-                            </li>
-                        </ul>
+                                    <li class="nav-item" >
+                                    <a class="nav-link" href="#">Register</a>
+                                    </li>
+                                </>
+                                }
 
+                                {
+                                    localStorage.getItem('user-info') ?    
+                                        <Navbar>
+                                                    
+                                            <Nav>
+                                                <NavDropdown title={user && user.firstname} className="btn btn-primary" style={{color:"white"}}>
+                                                    <NavDropdown.Item className="dropdown-item"onClick={logOut}>Logout</NavDropdown.Item>
 
+                                                    <NavDropdown.Item className="dropdown-item"onClick={profile}>Profile</NavDropdown.Item>
+                                                </NavDropdown>       
+                                            </Nav>
+                                        </Navbar>
+                                    :null  
+                                }
+                            </ul>
+                        </div>
+                                            
                     </div>
                 </div>
+
+                            
             </nav>
 
-
-
-
-
-
-
-
-
-           
+ 
         </div>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,13 +2,32 @@ import React from "react";
 
 import NavBar from '../components/NavBar';
 // import React from 'react';
-// import HomepageLeft from './HomepageLeft';
-// import HomepageRight from './HomepageRight';
+import '../styles/homepage.css';
+import '../styles/button.scss';
+import Contact from "../images/contact-tracing.gif";
+
+import{Link} from 'react-router-dom';
+
+
 // import Footer from './Footer';
 
 
 function Homepage()
 {
+
+  
+  let user= JSON.parse(localStorage.getItem('user-info'))
+    
+
+  function registerbutton() {
+      document.getElementById("registercontainer").style.display ="block";
+      document.getElementById("logincontainer").style.display ="none";
+    }
+
+    function loginbutton() {
+      document.getElementById("logincontainer").style.display ="block";
+      document.getElementById("registercontainer").style.display ="none";
+    }
 
 
 
@@ -19,18 +38,105 @@ function Homepage()
           <div className="piodos-navigation-items">
             <NavBar/>
           </div>
-          {/* <div className="container-fluid piodos-body-content" style={{marginBottom:"6%"}}>
-            <div className='container-fluid col-lg-6 p-0'>
-              <HomepageLeft/>
-            </div>
 
-            <div className='container-fluid col-lg-6 p-0'>
-              <HomepageRight />
-            </div> 
+          <div className="container-fluid piodos-body-content" style={{marginBottom:"6%"}}>
+
+            <div className="row p-5">
+                  <div className="col-lg-6 d-flex flex-column justify-content-center ms-5 mt-3">
+                      <h1 className="display-2" style={{fontWeight:"500"}}>
+                          Welcome to MLGCL COVID-19 Contact Tracing System!
+                      </h1>
+
+                      <h6 className="subhead pt-4" style={{color:"#737475" }}>
+                          MLGCL COVID-19 Contact Tracing System is the MLGCL's unofficial contact tracing system. We aim to manage the spread of COVID-19 to keep our students, instructors and staffs safe and healthy.
+                      </h6>
+
+                      {/* logincontainer */}
+                      <div className="logincontainer mt-5" id="logincontainer">
+
+                          <h1>
+                              I'd like to <span className="fw-bold" style={{color:"#06ABD5"}}>login</span> as an
+                          </h1>
+
+                          <div className="mt-5 mb-4 d-flex">
+                            
+                              {/* <button class="learn-more">
+                              <span class="circle" aria-hidden="true">
+                                <span class="icon arrow"></span>
+                              </span>
+                              <span class="button-text">Individual</span>
+                              </button> */}
+
+
+                                <div className="me-5">
+                                  <a href="/login" class="btn btn-white btn-animate">Individual</a>
+                                </div>
+
+                                <div className="ms-2">
+                                  <a href="/login" class="btn btn-white btn-animate">Establishment</a>
+                                </div>
+                                
+                             
+                             
+
+
+
+                              {/* <Link to="/login">
+                              <button className="btn logincontainerhover">Individual</button>
+                              </Link>
+
+                              <Link to="/login">
+                              <button className="btn ms-3 logincontainerhover">Admin</button>
+                              </Link> */}
+                          </div>
+                      
+
+                          <h6 className="mt-5">
+                              Don't have an account yet? <span className="fw-bold" style={{color:"#06ABD5", cursor:"pointer"}} onClick={registerbutton}>Register Now</span>
+                          </h6>
+
+                      </div>
+
+
+                      {/* registercontainer */}
+                      <div className="registercontainer mt-5" id="registercontainer">
+                                  
+                      <h6>
+                              I'd like to <span className="fw-bold" style={{color:"#06ABD5"}}>register</span> as an
+                          </h6>
+
+                          <div className="mt-4 mb-4">
+                              <Link to="/register">
+                              <button className="btn register-btn registercontainerhover" >Individual</button>
+                              </Link>
+
+                              <Link to="/register">
+                              <button className="btn ms-3 register-btn registercontainerhover">Admin</button>
+                              </Link>
+                          </div>
+                      
+
+                          <h6 className="mt-4">
+                              Already have an account? <span className="fw-bold" style={{color:"#06ABD5", cursor:"pointer"}} onClick={loginbutton}>Login Now</span>
+                          </h6>
+
+
+                      </div>
+
+      
+                      </div>
+                          <div className="col-lg-5 mt-5 pt-5 overflow">
+                          <img src={Contact} width="110%" />
+
+
+                  </div>
+              </div>
+
                    
           </div>
 
-          <div className="col piodos-footer-part" style={{position:"relative", bottom:"0px", width:"100%"}}>
+
+          {/* <div className="col piodos-footer-part" style={{position:"relative", bottom:"0px", width:"100%"}}>
             <Footer />
           </div> */}
         </div>
